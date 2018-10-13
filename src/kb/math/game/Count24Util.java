fde9479bf42996d7e39d1ad4ca115d18cc3e9611
zhuwei <382447688@qq.com>
2018-10-13 10:35:44 +0800

    完善计算方式。

--- src/kb/math/game/Count24Util.java
+++ src/kb/math/game/Count24Util.java
@@ -1,40 +1,26 @@
 package kb.math.game;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Count24Util {
 
   public static void main(String[] args) {
-    System.out.println(calc24("9 9 3 7"));
+    calc24(1, 2, 3, 4);
   }
   
-  static String calc24 (String in24) {
-    
-    String[] arr = in24.split("\\s+");
-    if (4 != arr.length) {
-      return "格式非法";
-    }
-    
-    int[] nums4 = new int[4];
-    try {
-      for (int i = 0; i < nums4.length; ++i) {
-        nums4[i] = Integer.parseInt(arr[i]);
-      }
-    } catch (NumberFormatException e) {
-      return "格式非法";
-    }
-    
-    ArrayList<String> result = calc(nums4, 24);
-    
-    if ((null == result) || (0 == result.size())) {
-      return "无可用组合";
-    }
+  static String calc24 (int ...in24) {
     
     StringBuffer sb = new StringBuffer();
-    for (String str : result) {
-      sb.append(str + "\r\n");
+    for (Temp temp : calcAll(in24)) {
+      if (24 == temp.getResult()) {
+        sb.append(""+temp.getCalc() + "\r\n");
+      }
     }
-    
+
+    if ("".equals(sb.toString())) {
+      sb.append("无可用组合");
+    }
     return sb.toString();
     
   }
@@ -54,9 +40,11 @@ public class Count24Util {
    *                                                 (((9/9)+7)*3)
    * @throws
    */
-  private static ArrayList<String> calc(int[] nums, int need) {
+  @SuppressWarnings("unused")
+  @Deprecated
+  private static Set<String> calc(int[] nums, int need) {
     
-    ArrayList<String> retArrStr = new ArrayList<String>();
+    Set<String> retArrStr = new HashSet<String>();
     
     if (1 == nums.length) {
       if (nums[0] == need) {
@@ -94,5 +82,141 @@ public class Count24Util {
     
     return retArrStr;
   }
+  
+  private static Set<Temp> calcAll(int... nums) {
+    
+    Set<Temp> retArrStr = new HashSet<Temp>();
+    
+    int size = nums.length;
+    assert size <= 4 : "Size Bigger Than 4";
 
+    switch (size) {
+    case 1:
+      Temp temp = new Temp();
+      temp.setCalc("" + nums[0] + "");
+      temp.setResult(nums[0]);
+      retArrStr.add(temp);
+      return retArrStr;
+    case 2:
+      Set<Temp> a = calcAll(nums[0]);
+      Set<Temp> b = calcAll(nums[1]);
+      retArrStr.addAll(combine(a, b));
+      break;
+    case 3:
+      //1+2
+      //nums[0] nums[1] nums[2] 
+      a = calcAll(nums[0]);
+      b = calcAll(nums[1], nums[2]);
+      retArrStr.addAll(combine(a, b));
+      //nums[1] nums[0] nums[2] 
+      a = calcAll(nums[1]);
+      b = calcAll(nums[0], nums[2]);
+      retArrStr.addAll(combine(a, b));
+      //nums[2] nums[0] nums[1] 
+      a = calcAll(nums[2]);
+      b = calcAll(nums[0], nums[1]);
+      retArrStr.addAll(combine(a, b));
+      break;
+    case 4:
+      //1+3
+      //nums[0] nums[1] nums[2] nums[3] 
+      a = calcAll(nums[0]);
+      b = calcAll(nums[1], nums[2], nums[3]);
+      retArrStr.addAll(combine(a, b));
+      //nums[1] nums[0] nums[2] nums[3] 
+      a = calcAll(nums[1]);
+      b = calcAll(nums[0], nums[2], nums[3]);
+      retArrStr.addAll(combine(a, b));
+      //nums[2] nums[0] nums[1] nums[3] 
+      a = calcAll(nums[2]);
+      b = calcAll(nums[0], nums[1], nums[3]);
+      retArrStr.addAll(combine(a, b));
+      
+      //2+2
+      // 01 23
+      a = calcAll(nums[0], nums[1]);
+      b = calcAll(nums[2], nums[3]);
+      retArrStr.addAll(combine(a, b));
+      // 02 13
+      a = calcAll(nums[0], nums[2]);
+      b = calcAll(nums[1], nums[3]);
+      retArrStr.addAll(combine(a, b));
+      // 03 12
+      a = calcAll(nums[0], nums[3]);
+      b = calcAll(nums[1], nums[2]);
+      retArrStr.addAll(combine(a, b));
+      break;
+    }
+    
+    return retArrStr;
+    
+  }
+
+  private static Set<Temp> combine(Set<Temp> a, Set<Temp> b) {
+    
+    Set<Temp> retArrStr = new HashSet<Temp>();
+
+    for (Temp aa : a) {
+      for (Temp bb : b) {
+        Temp temp = new Temp();
+        temp.setCalc("(" + aa.getCalc() + "+" + bb.getCalc() + ")");
+        temp.setResult(aa.getResult() + bb.getResult());
+        retArrStr.add(temp);
+        temp = new Temp();
+        temp.setCalc("(" + aa.getCalc() + "-" + bb.getCalc() + ")");
+        temp.setResult(aa.getResult() - bb.getResult());
+        retArrStr.add(temp);
+        temp = new Temp();
+        temp.setCalc("(" + aa.getCalc() + "*" + bb.getCalc() + ")");
+        temp.setResult(aa.getResult() * bb.getResult());
+        retArrStr.add(temp);
+        if ((bb.getResult() != 0)&&((aa.getResult() % bb.getResult()) == 0)){
+          temp = new Temp();
+          temp.setCalc("(" + aa.getCalc() + "/" + bb.getCalc() + ")");
+          temp.setResult(aa.getResult() / bb.getResult());
+          retArrStr.add(temp);
+        }
+
+        temp = new Temp();
+        temp.setCalc("(" + bb.getCalc() + "+" + aa.getCalc() + ")");
+        temp.setResult(bb.getResult() + aa.getResult());
+        retArrStr.add(temp);
+        temp = new Temp();
+        temp.setCalc("(" + bb.getCalc() + "-" + aa.getCalc() + ")");
+        temp.setResult(bb.getResult() - aa.getResult());
+        retArrStr.add(temp);
+        temp = new Temp();
+        temp.setCalc("(" + bb.getCalc() + "*" + aa.getCalc() + ")");
+        temp.setResult(bb.getResult() * aa.getResult());
+        retArrStr.add(temp);
+        if ((aa.getResult() != 0)&&((bb.getResult() % aa.getResult()) == 0)){
+          temp = new Temp();
+          temp.setCalc("(" + bb.getCalc() + "/" + aa.getCalc() + ")");
+          temp.setResult(bb.getResult() / aa.getResult());
+          retArrStr.add(temp);
+        }
+      }
+    }
+
+    return retArrStr;
+  }
+  
+
+}
+
+class Temp {
+  String calc;//表达式
+  int result;//结果
+  public String getCalc() {
+    return calc;
+  }
+  public void setCalc(String calc) {
+    this.calc = calc;
+  }
+  public int getResult() {
+    return result;
+  }
+  public void setResult(int result) {
+    this.result = result;
+  }
 }
